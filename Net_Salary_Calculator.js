9b9108b0c955be773ec9d3d027695808086ff2a6
Nicole Apono <napononicole@gmail.com>
2024-03-23 13:39:12 +0300

    Net_Salary_Calculator.js

--- Net_Salary_Calculator.js
+++ Net_Salary_Calculator.js
@@ -16,3 +16,50 @@ function calculateTax(grossSalary) {
     }
 }
 return tax;
+
+// Function to calculate NHIF deductions based on the provided rates
+function calculateNHIF(grossSalary) {
+    let nhif = 0;
+    if (grossSalary < 6000) {
+        nhif = 150;
+    } else if (grossSalary <= 8000) {
+        nhif = 300;
+    } else if (grossSalary <= 12000) {
+        nhif = 400;
+    } else if (grossSalary <= 15000) {
+        nhif = 500;
+    } else if (grossSalary <= 20000) {
+        nhif = 600;
+    } else if (grossSalary <= 25000) {
+        nhif = 750;
+    } else if (grossSalary <= 30000) {
+        nhif = 850;
+    }else if (grossSalary <= 35000) {
+        nhif = 900;
+    } else if (grossSalary <= 40000) {
+        nhif = 950;
+    } else if (grossSalary <= 45000) {
+        nhif = 1000;
+    } else if (grossSalary <= 50000) {
+        nhif = 1100;
+    } else if (grossSalary <= 60000) {
+        nhif = 1200;
+    } else if (grossSalary <= 70000) {
+        nhif = 1300;
+    } else if (grossSalary <= 80000) {
+        nhif = 1400;
+    } else if (grossSalary <= 90000) {
+        nhif = 1500;
+    } else if (grossSalary <= 100000) {
+        nhif = 1600;
+    } else {
+        nhif = 1700;
+    }
+    return nhif;
+
+// Function to calculate NSSF deductions based on provided rates
+function calculateNSSF(grossSalary) {
+    const nssfRate = 0.06; // the NSSF rate is 6% of the gross salary
+    return (grossSalary * nssfRate)
+}
+}
